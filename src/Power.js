677607eb37fda66a46c88af7fcba52163abb3cc3
EdenSweden <cope.eden@gmail.com>
2021-03-27 16:27:06 +1100

    fixed font awesome power icon

--- src/Power.js
+++ src/Power.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Power.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 
 function Power(){
 
     return (
         <div id="power">
             <button id="power-button">
-              <i className="fa fa-power-off" aria-hidden="true"></i>
+              <FontAwesomeIcon icon={faPowerOff} aria-hidden='true' />
+              {/*<i className="fa fa-power-off" aria-hidden="true"></i>*/}
             </button>
         </div>
         );
