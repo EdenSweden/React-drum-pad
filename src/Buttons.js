b5c65b88c837a954488ef4f5d02c1750a9762a0d
EdenSweden <cope.eden@gmail.com>
2021-03-28 18:50:16 +1100

    added audio links

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Buttons.css';
-//edit buttons code here
-const buttons = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+
+const buttons = [{letter: "Q", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"}, {letter: "W", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"}, {letter: "E", url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"}, {letter: "A", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"}, {letter: "S", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"}, {letter: "D", url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"}, {letter: "Z", url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"}, {letter: "X", url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"}, {letter: "C", url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"}];
 
 
 function Buttons(){
+
+const audioRef = useRef();
+const handleAudio = () => {
+    audioRef.current.play();
+}
+//use audioRef.current as ref, or audioRef as ref below?
 return(
 <div id="button-container">
-    {buttons.map((btn) => <button className="drum-pad" id={btn}>{btn}</button>)}
+    {buttons.map((btn) => <button onClick={handleAudio} className="drum-pad" id={btn.letter}>{btn.letter}<audio ref={audioRef} src={btn.url} preload /></button>)}
 </div>
 )
 
