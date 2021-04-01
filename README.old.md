c8f231163e112fc4fc0a56434b3d2175d4efb102
EdenSweden <cope.eden@gmail.com>
2021-04-01 21:05:39 +1100

    small changes from yesterday

--- README.old.md
+++ README.old.md
@@ -3,13 +3,13 @@ Drum pad project made for freeCodeCamp, but made in VS Code. Still in progress. 
 
 Personal note for changes needed:
 
-1. Assign proper audio links to buttons
-2. Add onKeyDown event listeners
-3. Create array of objects for second drum kit
-4. Add kit switching functionality
-5. Add display functionality
-6. Add power button functionality
-7. Add volume functionality
-8. Make adjustments for mobile responsiveness
-9. Make any needed adjustments to pass freeCodeCamp tests
-10. Finishing touches
+
+1. Add onKeyDown event listeners
+2. Create array of objects for second drum kit
+3. Add kit switching functionality
+4. Add display functionality
+5. Add power button functionality
+6. Add volume functionality
+7. Make adjustments for mobile responsiveness
+8. Make any needed adjustments to pass freeCodeCamp tests
+9. Finishing touches
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,21 +1,47 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './Buttons.css';
 
-const buttons = [{letter: "Q", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"}, {letter: "W", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"}, {letter: "E", url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"}, {letter: "A", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"}, {letter: "S", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"}, {letter: "D", url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"}, {letter: "Z", url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"}, {letter: "X", url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"}, {letter: "C", url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"}];
+const buttons = [{letter: "Q", keyCode: 81, url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"}, {letter: "W", keyCode: 87, url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"}, {letter: "E", keyCode: 69, url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"}, {letter: "A", keyCode: 65, url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"}, {letter: "S", keyCode: 83, url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"}, {letter: "D", keyCode: 68, url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"}, {letter: "Z", keyCode: 90, url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"}, {letter: "X", keyCode: 88, url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"}, {letter: "C", keyCode: 67, url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"}];
 
 
 function Buttons(){
 
+/*useEffect(() => {
+//IT WORKS WHEN YOU ADD EVENT LISTENER
+    window.addEventListener('keydown', handleAudioKeyDown);
+
+    return () => {window.removeEventListener('keydown', handleAudioKeyDown);
+};
+});*/
 
 const audioRef = useRef([]);
 //currently only plays the audio from the last url for all buttons.
-const handleAudio = (index) => {
+const handleAudioClick = (index) => {
     audioRef.current[index].play();
 };
+const testingKeyDown = () => {
+    console.log("keydown detected");
+}
+/*const handleAudioKeyDown = (event, index) => {
+    
+    buttons.forEach(btn => {console.log(btn.keyCode)
+        if(btn.keyCode === event.keyCode){
+            console.log("yay!");
+            audioRef.current[index].play();
+        }
+    });
+    //buttons.forEach(btn => console.log(btn.keyCode === event.keyCode););
+};*/
+
+/*const detectKey = (event, i) => {
+    if (event.keyCode === buttons[i].keyCode) {
+        handleAudio();
+    } 
+}*/
 
 return(
-<div id="button-container">
-    {buttons.map((btn, index) => <button key={btn.letter} onClick={() => handleAudio(index)} className="drum-pad" id={btn.letter}>{btn.letter}
+<div id="button-container" tabIndex={0} onKeyDown={testingKeyDown}>
+    {buttons.map((btn, index) => <button key={btn.letter} onClick={() => handleAudioClick(index)} onKeyDown={testingKeyDown} className="drum-pad" id={btn.letter}>{btn.letter}
         <audio ref={(ele) => audioRef.current.push(ele)} src={btn.url} />
     </button>)}
 </div>
