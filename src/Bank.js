69aa186ed387812020ee7e4c3ac43b1cad0aa663
EdenSweden <19919725+EdenSweden@users.noreply.github.com>
2021-05-09 18:12:29 +1000

    some responsive updates

--- src/Bank.js
+++ src/Bank.js
@@ -10,8 +10,8 @@ const dispatch = useContext(DispatchContext);
 
   return (
     <div id="bank">
-      <div id="bank-switch-holder" onClick={()=>dispatch({type: ACTIONS.TOGGLE_BANK})}>
-        <input title="switch sound bank" type="button" aria-label={state.isPowerOn ? state.drumKitData.displayText +" is active. Switch sound bank." : "Switch sound bank. Power is off."} className={state.kitOneIsActive ? "switched" : null } id="bank-switch"></input>
+      <div title="switch sound bank" id="bank-switch-holder" onClick={()=>dispatch({type: ACTIONS.TOGGLE_BANK})}>
+        <input type="button" aria-label={state.isPowerOn ? state.drumKitData.displayText +" is active. Switch sound bank." : "Switch sound bank. Power is off."} className={state.kitOneIsActive ? "switched" : null } id="bank-switch"></input>
       </div>
     </div>
   );
